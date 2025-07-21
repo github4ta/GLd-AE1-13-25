8b6d523cc3b98c318b4cf5a1eb6fb566f7f41cb6
Natallia Laptsionak <natalialaptsionak@gmail.com>
2025-07-21 21:33:52 +0300

    Add CalculatorLaptsionak

--- CalculatorLaptsionak.java
+++ CalculatorLaptsionak.java
@@ -1,17 +1,17 @@
 public class CalculatorLaptsionak {
-    public int sub(int a, int b) {  // no usages new *
+    public int sub(int a, int b) {
         return a + b;
     }
 
-    public int minus(int a, int b) { // no usages new *
+    public int minus(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) { // no usages new *
+    public int multiply(int a, int b) {
         return a * b;
     }
 
-    public double devide(int a, int b) { // no usages new *
+    public double devide(int a, int b) {
         return a / b;
     }
 }
