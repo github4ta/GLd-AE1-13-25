c7dd4bd04a8f02c53f1cc638b8d7b48064b2c9f5
Polina Bogdanovich <yapolli10@gmail.com>
2025-07-21 22:03:42 +0300

    add method printHelloBogdanovich()

--- Utils.java
+++ Utils.java
@@ -1,5 +1,10 @@
 public class Utils {
-    public void printHelloBogdanovich() {
-        System.out.println("Hello from Bogdanovich");
+
+    public void printHelloFromTsarik() {
+        System.out.println("Hello from Tsarik!");
+    }
+
+    public void printHelloFromBogdanovich() {
+        System.out.println("Hello from Bogdanovich!");
     }
 }
