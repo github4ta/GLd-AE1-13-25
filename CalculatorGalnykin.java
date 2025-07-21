0cd0906a16c182163e9df7cfee40373d16ab9c77
Sergey Galnykin <galnykin@gmail.com>
2025-07-21 21:29:34 +0300

    add class CalculatorGalnykin

---
+++ CalculatorGalnykin.java
@@ -0,0 +1,17 @@
+public class CalculatorGalnykin {
+    public int sub(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int a) {
+        return a * b;
+    }
+
+    public double devide(int a, int b) {
+        return a / b;
+    }
+}
