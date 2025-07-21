3a5374abb7705a0e771875eb5a8365b379c8eeeb
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-07-21 22:01:30 +0300

    add hello Lapitskaya

--- Utils.java
+++ Utils.java
@@ -1,6 +1,10 @@
 public class Utils {
 
     public void printHelloFromTsarik() {
+        System.out.println("Hello from Tsarik!");
+    }
+
+    public void printHelloFromLapitskaya() {
         System.out.println("Hello from Lapitskaya!");
     }
 }
