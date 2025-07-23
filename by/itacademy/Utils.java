5e8c51bec35aa7346b62652a60ed4f40b791d959
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-07-23 21:18:16 +0200

    add getRandomStringByNumber

--- by/itacademy/Utils.java
+++ by/itacademy/Utils.java
@@ -1,6 +1,16 @@
 package by.itacademy;
 
+import java.util.Random;
+
 public class Utils {
 
-
+    public static String getRandomStringByNumber(int length) {
+        String str = "qwertyuiopasdfghjk1234567890";
+        Random random = new Random();
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result = result + str.charAt(random.nextInt(str.length()));
+        }
+        return result;
+    }
 }
