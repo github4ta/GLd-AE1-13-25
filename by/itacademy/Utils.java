73619dbabda215117b9ef2a87f4f77a085b69cfc
Natallia Laptsionak <natalialaptsionak@gmail.com>
2025-07-23 22:00:07 +0300

    add generateEmail

--- by/itacademy/Utils.java
+++ by/itacademy/Utils.java
@@ -1,6 +1,8 @@
 package by.itacademy;
 
 public class Utils {
-
-
+    public static String generateEmail(String name, String surname) {
+        String email = name.toLowerCase() + "." + surname.toLowerCase() + "@gmail.com";
+        return email;
+    }
 }
