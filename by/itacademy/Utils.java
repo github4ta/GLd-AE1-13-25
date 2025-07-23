023557fb84d3d4e8dab03411ed11b569766c66a8
Cath-Zav <cathzavizion@gmail.com~>
2025-07-23 20:55:57 +0200

    refactor

--- by/itacademy/Utils.java
+++ by/itacademy/Utils.java
@@ -4,10 +4,11 @@ public class Utils {
     public static String capitalLetter(String str) {
         return str.substring(0, 1).toUpperCase();
     }
-        public static int getDayFromDate(String date) {
-          String str = date.substring(3, 5);
-          return Integer.valueOf(str);
-        }
+
+    public static int getDayFromDate(String date) {
+        String str = date.substring(3, 5);
+        return Integer.valueOf(str);
+    }
 
     public static String getDate(int day, int month, int year) {
         String stringDay = String.format("%02d", day);
