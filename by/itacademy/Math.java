1343b23aa88a5795e11a09d9221844e1dadab2df
Gleb Tolstik <glebtolst1k@mail.ru>
2025-07-23 20:29:48 +0300

    add methods getAreaOfRectangle

--- by/itacademy/Math.java
+++ by/itacademy/Math.java
@@ -2,4 +2,8 @@ package by.itacademy;
 
 public class Math {
 
+    public static void getAreaOfRectangle(int a, int b) {
+        int p = 2 * (a + b);
+        System.out.println("Периметр прямоугольника " + p);
+    }
 }
