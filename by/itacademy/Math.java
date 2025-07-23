24478d3a4d76e2ebfac72741641efd3f9154b754
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-07-23 19:39:14 +0200

    refactor code

--- by/itacademy/Math.java
+++ by/itacademy/Math.java
@@ -15,4 +15,7 @@ public class Math {
     public double trapezoidArea(int a, int b, int height) {
         return (double) ((a + b) * height) / 2;
     }
+
+    public static int rectangleArea(int a, int b) {
+        return a * b;
 }
