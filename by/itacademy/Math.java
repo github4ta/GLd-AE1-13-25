4de8ee98455308715d6f5e3a5fc54d9f2f2016cc
Gleb Tolstik <glebtolst1k@mail.ru>
2025-07-23 20:50:03 +0300

    refactor methods

--- by/itacademy/Math.java
+++ by/itacademy/Math.java
@@ -5,9 +5,8 @@ public class Math {
         return a + b + c;
     }
 
-    public static void getAreaOfRectangle(int a, int b) {
-        int p = 2 * (a + b);
-        System.out.println("Периметр прямоугольника " + p);
+    public static int gePerimeterOfRectangle(int a, int b) {
+        return 2 * (a + b);
     }
 
     public static double trapezoidArea(int a, int b, int height) {
