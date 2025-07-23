c2dde035e3fe39fae00c0070154ec965a1ab6551
Polina Bogdanovich <yapolli10@gmail.com>
2025-07-23 20:34:10 +0300

    add method for calculation a perimeter of triangle

--- by/itacademy/Math.java
+++ by/itacademy/Math.java
@@ -1,5 +1,9 @@
 package by.itacademy;
 
 public class Math {
-
+    public static int perimeterOfTriangle(int a, int b, int c) {
+        int perimeter = a + b + c;
+        System.out.println("Perimeter of triangle: " + perimeter);
+        return perimeter;
+    }
 }
