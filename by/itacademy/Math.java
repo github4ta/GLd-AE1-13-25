84429bad9317ed5fb72561f13895fd71fef486c5
Natallia Laptsionak <natalialaptsionak@gmail.com>
2025-07-23 20:49:46 +0300

    add triangleArea

--- by/itacademy/Math.java
+++ by/itacademy/Math.java
@@ -1,5 +1,9 @@
 package by.itacademy;
 
 public class Math {
-
+    public static double triangleArea(int a, int b, int c) {
+        double s = (a + b + c) / 2.0;
+        double area = java.lang.Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return area;
+    }
 }
