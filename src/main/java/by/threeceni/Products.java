54849d334bf33392a149f3651e717f111f4c8be1
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-07-24 14:51:11 +0200

    add getRandomChocolateCandy()

--- src/main/java/by/threeceni/Products.java
+++ src/main/java/by/threeceni/Products.java
@@ -16,4 +16,38 @@ public class Products {
         Random random = new Random();
         return allJellyCandy[random.nextInt(allJellyCandy.length)];
     }
+
+
+    public static String getRandomChocolateCandy() {
+        String[] chocolateCandies = {
+                "Добрый злак с белым шоколадом",
+                "Добрый Злак с белым шоколадом и вкусом вишни",
+                "Шокозаврики",
+                "Чудо-Чудное с какао Вишневый",
+                "Конфеты со вкусом лесного ореха CHIQOLA Сердце",
+                "Конфеты со вкусом лесного ореха CHIQOLA Треугольник",
+                "Ozera c фундуком",
+                "АЭРОФЛОТСКИЕ 35 гр",
+                "Аленка 35 гр",
+                "АЭРОФЛОТСКИЕ 200 гр",
+                "Ярче",
+                "Super с нугой и мягкой карамелью",
+                "LOIS с клубничной нугой",
+                "Крокант",
+                "Ozera c ореховой начинкой",
+                "Адель с цельным миндалем",
+                "MILKIDS c молочной начинкой",
+                "MILKIDS c молочно-клубничной начинкой",
+                "Лукоморье",
+                "Ozera суфле банан",
+                "Baby Fox creamy choco",
+                "Орешка",
+                "Merci",
+                "Рафаэлло"
+        };
+
+        Random random = new Random();
+        int index = random.nextInt(chocolateCandies.length);
+        return chocolateCandies[index];
+    }
 }
