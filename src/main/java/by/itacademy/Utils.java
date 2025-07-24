d3939eadd10dbd486cb462e96d6a2c03fab2c2a3
Sergei Tsarik <serge.tsarik@gmail.com>
2025-07-24 14:24:05 +0300

    refactor code

--- src/main/java/by/itacademy/Utils.java
+++ src/main/java/by/itacademy/Utils.java
@@ -1,38 +1,57 @@
+package by.itacademy;
+
+import java.util.Random;
+
 public class Utils {
-    public static void printHelloFromZavizion() {
-        System.out.println("Hello from Zavizion!");
+    public static String capitalLetter(String str) {
+        return str.substring(0, 1).toUpperCase();
+    }
+        public static int getDayFromDate(String date) {
+          String str = date.substring(3, 5);
+          return Integer.valueOf(str);
+        }
+
+    public static String getDate(int day, int month, int year) {
+        String stringDay = String.format("%02d", day);
+        String stringMonth = String.format("%02d", month);
+        String stringYear = String.valueOf(year);
+        return stringMonth + "-" + stringDay + "-" +stringYear;
     }
 
-    public static void printHelloTsarik() {
-        System.out.println(" Hello from Sergei");
+    public static String generateEmail(String name, String surname) {
+        String email = name.toLowerCase() + "." + surname.toLowerCase() + "@gmail.com";
+        return email;
     }
 
-    public static void printHelloFromGalnykin() {
-        System.out.println("Hello from Galnykin!");
+    public static boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
-    public static void printHelloFromBogdanovich() {
-        System.out.println("Hello from Bogdanovich!");
+    public static String lineInput(String str) {
+        return str.substring( 1).toUpperCase();
     }
 
-    public static void printHelloFromSukhadolskaya() {
-        System.out.println("Hello from Sukhadolskaya!");
-    }
-
-    public static void printHelloFromTolstik() {
-        System.out.println("Hello from Tolstik!");
-    }
-
-    public static void printHelloFromLaptsionak() {
-        System.out.println("Hello from Laptsionak!");
-
-    }
-
-    public static void printHelloFromLapitskaya() {
-        System.out.println("Hello from Lapitskaya!");
-    }
-
-    public static void sprintHelloFromKlimovich(){
-        System.out.println("Hello from Klimovich!");
+    public static String getRandomStringByNumber(int length) {
+        String str = "qwertyuiopasdfghjk1234567890";
+        Random random = new Random();
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result = result + str.charAt(random.nextInt(str.length()));
+        }
+        return result;
     }
 }
