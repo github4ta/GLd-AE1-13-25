d978b08e549d1930fcd94383ba820e1b9235d776
Sergei Tsarik <serge.tsarik@gmail.com>
2025-07-30 19:52:15 +0300

    refactor code

--- src/main/java/by/itacademy/CalculatorTest.java
+++ src/main/java/by/itacademy/CalculatorTest.java
@@ -1,14 +1,16 @@
 package by.itacademy;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
     @Test
+    @DisplayName("Проверка метода сложения 2 + 5 = 7")
     public void test1() {
         Calculator calculator = new Calculator();
         int actual = calculator.add(2, 5);
-        Assertions.assertEquals(actual, 8);
+        Assertions.assertEquals(actual, 7);
     }
 }
