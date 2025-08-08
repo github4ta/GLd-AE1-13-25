8be7352e9d5e999c52e756a053ee7061e548a5ab
Sergey Galnykin <galnykin@gmail.com>
2025-08-08 16:02:58 +0300

    add CHECKBOX_SELECT_ALL, CHECKBOX_LABEL_SELECT_ALL fields

--- src/main/java/by/emall/CartPage.java
+++ src/main/java/by/emall/CartPage.java
@@ -1,4 +1,9 @@
 package by.emall;
 
 public class CartPage {
+    private static final String CHECKBOX_SELECT_ALL =
+            "//span[@class='checkbox_checkbox__pseudo__Km_pz checkbox_checkbox__pseudo_checked__CNCMz']" +
+                    "/following-sibling::span[contains(text(), 'Выбрать всё')]";
+    private static final String CHECKBOX_LABEL_SELECT_ALL =
+            "//input[@type='checkbox']/following-sibling::span[contains(text(), 'Выбрать всё')]";
 }
