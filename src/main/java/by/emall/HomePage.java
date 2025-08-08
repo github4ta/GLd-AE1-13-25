f9ac3ed298637e59b02af330338486eb4f631af6
Sergey Galnykin <galnykin@gmail.com>
2025-08-08 15:16:49 +0300

    add FOOTER_LINK_FOR_SUPPLIERS field

--- src/main/java/by/emall/HomePage.java
+++ src/main/java/by/emall/HomePage.java
@@ -1,4 +1,9 @@
 package by.emall;
 
 public class HomePage {
+    private static final String CLASS_FOOTER_LINK = "@class='footer_link__kkAzq'";
+    private static final String FOOTER_LINK_TEXT_FOR_SUPPLIERS = "contains(text(),'Поставщикам')";
+
+    private static final String FOOTER_LINK_FOR_SUPPLIERS =
+            "//a[" + CLASS_FOOTER_LINK + " and " + FOOTER_LINK_TEXT_FOR_SUPPLIERS + "]";
 }
