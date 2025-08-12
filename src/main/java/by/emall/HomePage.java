bcc810833689f2f3ca2113288a94c417223626ce
Sergey Galnykin <galnykin@gmail.com>
2025-08-12 13:48:09 +0300

    refactor code for FOOTER_LINK_FOR_SUPPLIERS XPath field

--- src/main/java/by/emall/HomePage.java
+++ src/main/java/by/emall/HomePage.java
@@ -1,18 +1,15 @@
 package by.emall;
 
 public class HomePage {
-    private final String LINK_VACANCIES="//a[@href='https://jobs.e-dostavka.by/']";
-    private final String LINK_TELEGRAM="//a[@href='https://t.me/emall_by']";
-
-    private final String CLASS_FOOTER_LINK = "@class='footer_link__kkAzq'";
-    private final String FOOTER_LINK_TEXT_FOR_SUPPLIERS = "contains(text(),'Поставщикам')";
+    private final String LINK_VACANCIES = "//a[@href='https://jobs.e-dostavka.by/']";
+    private final String LINK_TELEGRAM = "//a[@href='https://t.me/emall_by']";
 
     private final String FOOTER_LINK_FOR_SUPPLIERS =
-            "//a[" + CLASS_FOOTER_LINK + " and " + FOOTER_LINK_TEXT_FOR_SUPPLIERS + "]";
+            "//a[@href='https://emall.by/information/company/conditions-for-selecting-a-counterparty']";
 
     private final String contactsLink = "//a[@title='Контакты']";
-    private final String HEADER_LOGIN_LINK="//a[@href='https://emall.by/login?backurl=/']";
-    private final String FOOTER_USER_AGREEMENT_LINK="//a[@href='https://api-preprod.emall.by/649008c9c6277_publichnyj-dogovor-emall.pdf']";
+    private final String HEADER_LOGIN_LINK = "//a[@href='https://emall.by/login?backurl=/']";
+    private final String FOOTER_USER_AGREEMENT_LINK = "//a[@href='https://api-preprod.emall.by/649008c9c6277_publichnyj-dogovor-emall.pdf']";
 
     private final String FOOTER_TEXT_ABOUT_SERVICE = "//a[text() = 'О сервисе']";
     private final String FOOTER_TEXT_AGREEMENT_CREDIT_RESOURCES = "//a[text() = 'Соглашение о кредитных ресурсах']";
