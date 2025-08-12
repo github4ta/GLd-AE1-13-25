238d00f8bab6e38ecc1313a331ea4d5b4ee22d45
Sergey Galnykin <galnykin@gmail.com>
2025-08-12 13:59:11 +0300

    refactor code for BUTTON_SUBMIT field

--- src/main/java/by/emall/LoginPage.java
+++ src/main/java/by/emall/LoginPage.java
@@ -3,8 +3,10 @@ package by.emall;
 public class LoginPage {
     private final String LABEL_PHONE_NUMBER="//label[@class='new-input-phone_label__BGsx4']";
 
-    private final String GET_A_CODE =
-            "//button[@class='btn_btn__jx6SQ btn_btn_colour_black__wifsv btn_btn_size_medium__ugU8B login_button__Ewgfi']";
+    private final String BUTTON_SUBMIT =
+            "//button[@class='btn_btn__jx6SQ btn_btn_colour_black__wifsv " +
+                    "btn_btn_size_medium__ugU8B login_button__Ewgfi']";
+
     private final String pageTitle = "//h1[text()='Вход']";
     private final String EMPTY_TEL_NUMBER_ERROR_MESSAGE = "//div[@class='new-input-phone_message__WsZQD']";
 
