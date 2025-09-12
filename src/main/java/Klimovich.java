0fc619358ae468c5b700ae848bbfa4e54d142186
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-09-12 10:28:17 +0300

    add method generateString add dependency Commons Lang 2.6

--- src/main/java/Klimovich.java
+++ src/main/java/Klimovich.java
@@ -1,9 +1,20 @@
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.util.Random;
+
 public class Klimovich {
 
     public static String generateString(int length) {
-        String generateString = RandomStringUtils.randomAlphabetic(length);
-        return generateString;
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return generatedString;
     }
 }
