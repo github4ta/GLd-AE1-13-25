b8ecb87c8743a24033fce82d2e47dfe421b62540
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-09-12 10:35:26 +0300

    add generateString

--- src/main/java/Klimovich.java
+++ src/main/java/Klimovich.java
@@ -6,11 +6,10 @@ public class Klimovich {
     public static String generateString(int length) {
         int leftLimit = 97;
         int rightLimit = 122;
-        int targetStringLength = 10;
         Random random = new Random();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .limit(targetStringLength)
+                .limit(length)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
 
