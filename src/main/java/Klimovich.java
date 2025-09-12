c0a79b2938285b437e0ba03aa20de1a72ed55fd6
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-09-12 12:05:59 +0300

    refactor Utils class add Klimovich class

--- src/main/java/Klimovich.java
+++ src/main/java/Klimovich.java
@@ -1,18 +1,3 @@
-
-import java.util.Random;
-
 public class Klimovich {
 
-    public static String generateString(int length) {
-        int leftLimit = 97;
-        int rightLimit = 122;
-        Random random = new Random();
-
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .limit(length)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        return generatedString;
-    }
 }
