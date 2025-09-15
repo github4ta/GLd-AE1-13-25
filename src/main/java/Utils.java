d02146e29ffdcba918d0d373e850d9469bdf7078
KlimovichSergey <78042203+KlimovichSergey@users.noreply.github.com>
2025-09-15 21:38:51 +0300

    PR0002 - Utils (#26)

---
+++ src/main/java/Utils.java
@@ -0,0 +1,17 @@
+
+import java.util.Random;
+
+public class Utils {
+
+    public static String generateString(int length) {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return generatedString;
+    }
+}
