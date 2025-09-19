88052d3aec2e3d8bbe953de1431e468a4e39898a
Gleb Tolstik <glebtolst1k@mail.ru>
2025-09-19 23:38:37 +0300

    add new web elements locators

--- src/main/java/by/vek/ui/pages/account/AccountLocator.java
+++ src/main/java/by/vek/ui/pages/account/AccountLocator.java
@@ -9,4 +9,7 @@ public class AccountLocator {
     public static final String BUTTON_CONTINUE = "//button[@class='Button-module__button EmailLoginForm_baseActionButton__5cptH Button-module__blue-primary']";
     public static final String BUTTON_REGISTRATION = "//button[@class='LinkButton-module__wrapper LinkButton-module__body LinkButton-module__regular LinkButton-module__blue']";
     public static final String BUTTON_FORGET_PASSWORD = "//button[@class='LinkButton-module__wrapper LinkButton-module__small LinkButton-module__regular LinkButton-module__blue']";
+    public static final String TITLE_TEXT_RESET_PASSWORD = "//h5[@class='Form-module__formTitle']";
+    public static final String BUTTON_BACK = "//button[@data-testid='modalBack']";
+    public static final String BUTTON_CLOSE = "//button[@data-testid='modalClose']";
 }
