c49af7e05fc8649b1176c0495047d74ddb741c96
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-09-19 21:40:08 +0200

    add BaseLogger, log4j2.xml and update pom.xml

---
+++ src/main/java/by/vek/utils/BaseLogger.java
@@ -0,0 +1,8 @@
+package by.vek.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class BaseLogger {
+    protected static final Logger logger= LogManager.getLogger();
+}
