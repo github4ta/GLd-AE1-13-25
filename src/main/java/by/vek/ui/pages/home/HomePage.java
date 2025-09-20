d734587decf2d8829f19cd22daedce75893c1a41
kate <cathzavizion@gmail.com>
2025-09-21 00:09:31 +0200

    - fix comments - add logger to try-catch instead of sout error message

--- src/main/java/by/vek/ui/pages/home/HomePage.java
+++ src/main/java/by/vek/ui/pages/home/HomePage.java
@@ -1,14 +1,13 @@
 package by.vek.ui.pages.home;
 
 import by.vek.ui.driver.Driver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class HomePage {
 
@@ -16,12 +15,13 @@ public class HomePage {
     }
 
     WebDriver driver = Driver.getDriver();
+    private static final Logger logger = LogManager.getLogger();
 
     public HomePage clickAcceptCookie() {
-        try{
+        try {
             driver.findElement(By.xpath(HomeLocator.BUTTON_ACCEPT_COOKIES)).click();
-        } catch(Exception e) {
-            System.out.println("There wasn't cookie banner. Cookie banner wasn't closed");
+        } catch (TimeoutException | NoSuchElementException e) {
+            logger.info("There wasn't cookie banner. Cookie banner wasn't closed");
         }
         return this;
     }
@@ -33,17 +33,16 @@ public class HomePage {
     }
 
     public void clickCart() {
-        Driver.getDriver().findElement(By.xpath(HomeLocator.BUTTON_CART)).click();
+        driver.findElement(By.xpath(HomeLocator.BUTTON_CART)).click();
     }
 
     public void startSearch(String itemToSearch) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        WebElement searchInput = Driver.getDriver().findElement(By.xpath(HomeLocator.INPUT_CATALOG_SEARCH));
+        WebElement searchInput = driver.findElement(By.xpath(HomeLocator.INPUT_CATALOG_SEARCH));
+        searchInput.click();
         searchInput.sendKeys(itemToSearch);
-        searchInput.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(HomeLocator.SEARCH_DROPDOWN)));
-        searchInput.click();
         searchInput.sendKeys(Keys.ENTER);
-   }
+    }
 }
