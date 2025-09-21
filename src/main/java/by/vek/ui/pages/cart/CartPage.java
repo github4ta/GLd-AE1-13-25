18e5c2567c9628aff3487bc5da7837e660f50998
Sergey Galnykin <galnykin@users.noreply.github.com>
2025-09-21 17:14:41 +0300

    UI0007 - CartPage – действия (#36)
    * add CartPage class
    
    * refactor CartPage class
    
    * refactor CartPage class:
    - use general import for CartLocators constants
    
    * refactor CartPage class:
    - change formatting
    
    * refactor CartPage class:
    - add WebDriverWait wait field
    - add WAIT_TIMEOUT field
    - add CartPage constructor with WebDriverWait object creation
    - add getVisibleElement method
    - add getVisibleElement for some element methods
    
    * refactor CartPage class:
    - add a parameter to CartPage(WebDriver driver) constructor
    - add driver initialisation to the constructor

---
+++ src/main/java/by/vek/ui/pages/cart/CartPage.java
@@ -0,0 +1,48 @@
+package by.vek.ui.pages.cart;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static by.vek.ui.pages.cart.CartLocators.*;
+
+public class CartPage {
+
+    private static final int WAIT_TIMEOUT = 10;
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT));
+    }
+
+    public String getEmptyCartText() {
+        return getElement(By.xpath(TITLE_CART_IS_EMPTY)).getText();
+    }
+
+    public void clickSelectAllCheckbox() {
+        getVisibleElement(By.xpath(CHECKBOX_SELECT_ALL)).click();
+    }
+
+    public String getTotalAmountText() {
+        return getVisibleElement(By.xpath(TOTAL_PRICE_VALUE)).getText();
+    }
+
+    public void clickCheckoutButton() {
+        getVisibleElement(By.xpath(BUTTON_PLACE_ORDER)).click();
+    }
+
+    private WebElement getElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    private WebElement getVisibleElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
