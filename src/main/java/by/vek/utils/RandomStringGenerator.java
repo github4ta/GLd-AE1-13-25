4327473163a9718b902b1f3ae980fa1dc7837f27
Natallia Laptsionak <natalialaptsionak@gmail.com>
2025-09-21 11:23:26 +0300

    Add RandomStringGenerator class (PR0004 - Utils – xX1)

---
+++ src/main/java/by/vek/utils/RandomStringGenerator.java
@@ -0,0 +1,18 @@
+package by.vek.utils;
+
+import java.security.SecureRandom;
+
+public class RandomStringGenerator {
+
+    public static String generateRandomAlphaNumericString(int length) {
+        final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+
+        return sb.toString();
+    }
+}
