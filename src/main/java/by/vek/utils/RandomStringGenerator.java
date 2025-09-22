334eafda9ea0bcf6ce8ae9a2fe63404972eff70a
Natallia Laptsionak <natalialaptsionak@gmail.com>
2025-09-22 09:10:17 +0300

    code refactor - RandomStringGenerator class (PR0004 - Utils – xX1)

--- src/main/java/by/vek/utils/RandomStringGenerator.java
+++ src/main/java/by/vek/utils/RandomStringGenerator.java
@@ -4,9 +4,14 @@ import java.security.SecureRandom;
 
 public class RandomStringGenerator {
 
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom random = new SecureRandom();
+
     public static String generateRandomAlphaNumericString(int length) {
-        final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        SecureRandom random = new SecureRandom();
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be greater than 0.");
+        }
+
         StringBuilder sb = new StringBuilder(length);
 
         for (int i = 0; i < length; i++) {
