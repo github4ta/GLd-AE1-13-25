332e3f51e0f5d7eea0c3bddc11592d0b1a3d763d
Cath-Zav <125591437+Cath-Zav@users.noreply.github.com>
2025-09-24 20:43:20 +0200

    Merge pull request #46
    * refactor SearchTest

--- src/test/java/com/zara/SearchTest.java
+++ src/test/java/com/zara/SearchTest.java
@@ -3,25 +3,32 @@ package com.zara;
 import com.zara.driver.Driver;
 import com.zara.pages.HomePage;
 import com.zara.pages.SearchPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SearchTest {
-
-    @Test
-    public void test() {
+    @BeforeEach
+    public void openSite() {
         HomePage homePage = new HomePage();
         homePage.openSite();
         homePage.clickButtonAcceptCookies();
         homePage.clickButtonCloseLocation();
 
         homePage.clickSearchButton();
+    }
 
+    @Test
+    public void test() {
         SearchPage searchPage = new SearchPage();
         searchPage.sendKeysInputSearch("qwertyu");
 
         Assertions.assertEquals("НИЧЕГО НЕ НАЙДЕНО", searchPage.getNotFoundText());
+    }
 
+    @AfterEach
+    public void tearDown() {
         Driver.quit();
     }
 }
