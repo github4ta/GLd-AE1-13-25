483670b7c9cd258d327496ee40b2bf79d96ff04a
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-09-24 19:22:29 +0200

    added BasePage

---
+++ src/main/java/com/zara/pages/BasePage.java
@@ -0,0 +1,23 @@
+package com.zara.pages;
+
+import com.zara.driver.Driver;
+import org.openqa.selenium.By;
+
+public class BasePage {
+    private final String URL = "https://www.zara.com/by/ru/";
+    private final String BUTTON_ACCEPT_COOKIES = "//button[@id='onetrust-accept-btn-handler']";
+    private final String BUTTON_CLOSE_LOCATION = "//button[@class='zds-dialog-icon-button zds-dialog-close-button']";
+    private final String BUTTON_ACCEPT_LOCATION = "//button[@data-qa-action='stay-in-store']";
+
+    public void openSite() {
+        Driver.getDriver().get(URL);
+    }
+
+    public void clickButtonAcceptCookies() {
+        Driver.getDriver().findElement(By.xpath(BUTTON_ACCEPT_COOKIES)).click();
+    }
+
+    public void clickButtonCloseLocation() {
+        Driver.getDriver().findElement(By.xpath(BUTTON_CLOSE_LOCATION)).click();
+    }
+}
