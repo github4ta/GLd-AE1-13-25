f0ce8898d93d5ebedd010ab144e8ac5ba4eca1be
NataliaLaptsionak <natalialaptsionak@gmail.com>
2025-09-24 21:22:36 +0300

    code refactor Add Search Test class (#44)

---
+++ src/test/java/com/zara/SearchTest.java
@@ -0,0 +1,27 @@
+package com.zara;
+
+import com.zara.driver.Driver;
+import com.zara.pages.HomePage;
+import com.zara.pages.SearchPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SearchTest {
+
+    @Test
+    public void test() {
+        HomePage homePage = new HomePage();
+        homePage.openSite();
+        homePage.clickButtonAcceptCookies();
+        homePage.clickButtonCloseLocation();
+
+        homePage.clickSearchButton();
+
+        SearchPage searchPage = new SearchPage();
+        searchPage.sendKeysInputSearch("qwertyu");
+
+        Assertions.assertEquals("Ничего не найдено", searchPage.getNotFoundText());
+
+        Driver.quit();
+    }
+}
