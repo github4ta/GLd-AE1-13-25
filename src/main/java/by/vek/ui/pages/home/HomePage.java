441e56fde0a6f1f1b9af84711045390a629ced38
Sergei Tsarik <serge.tsarik@gmail.com>
2025-09-27 13:58:52 +0300

    refactor

--- src/main/java/by/vek/ui/pages/home/HomePage.java
+++ src/main/java/by/vek/ui/pages/home/HomePage.java
@@ -1,12 +1,13 @@
 package by.vek.ui.pages.home;
 
 import by.vek.ui.driver.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.logging.LogManager;
 
 public class HomePage {
 
@@ -14,7 +15,7 @@ public class HomePage {
     }
 
     WebDriver driver = Driver.getDriver();
-    private static final LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public HomePage clickAcceptCookie() {
         try {
