0588ceac427af44d32e0313d4004339021d87068
gleb_tolstik <glebtolst1k@mail.ru>
2025-09-29 19:02:54 +0300

    UI0009-SearchLocators (#49)

--- src/main/java/by/vek/ui/pages/search/SearchLocator.java
+++ src/main/java/by/vek/ui/pages/search/SearchLocator.java
@@ -1,4 +1,8 @@
 package by.vek.ui.pages.search;
 
 public class SearchLocator {
+    private final String PRODUCT_CARD = "//div[@class='style_product__xVGB6']";
+    private final String CARD_PRICE = "//p[@data-testid='card-current-price']";
+    private final String CARD_NAME = "//a[@data-testid='card-info-a']";
+    private final String BUTTON_ADD_TO_BASKET = "//button[contains(@class,'style_basketButton__B81O2')]";
 }
