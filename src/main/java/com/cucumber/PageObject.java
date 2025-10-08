4745e1d242ba0774c8c58e84fda2f449b916d0ea
Sergei Tsarik <serge.tsarik@gmail.com>
2025-10-08 20:06:10 +0300

    add annotations

--- src/main/java/com/cucumber/PageObject.java
+++ src/main/java/com/cucumber/PageObject.java
@@ -1,19 +1,26 @@
 package com.cucumber;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
 public class PageObject {
 
+    @Given("User opens site")
     public void openSite() {
         System.out.println("Open Site");
     }
 
+    @Given("User clicks button Login")
     public void clickLogin() {
         System.out.println("Click Login");
     }
 
+    @When("User fills Email")
     public void fillEmail(String email) {
         System.out.printf("Fill Email - \n", email);
     }
 
+    @When("User clicks button Submit")
     public void clickSubmit() {
         System.out.println("Click Submit");
     }
